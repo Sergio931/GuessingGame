2c9f72be30cfd51dde233b2108de7a086a85f9b9
Sergio931 <75312430+Sergio931@users.noreply.github.com>
2022-03-14 22:04:01 +0300

    GuessingGame

---
+++ app/src/main/java/com/example/guessinggame/GameViewModel.kt
@@ -0,0 +1,67 @@
+package com.example.guessinggame
+import android.view.View
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+class GameViewModel : ViewModel(){
+    private val words = listOf("Android", "Activity", "Fragment")
+    private val secretWord = words.random().uppercase()
+    private val _secretWordDisplay = MutableLiveData<String>()
+    val secretWordDisplay: LiveData<String>
+        get() = _secretWordDisplay
+    private var correctGuesses = ""
+    private val _incorrectGuesses = MutableLiveData<String>("")
+    val incorrectGuesses: LiveData<String>
+        get() = _incorrectGuesses
+    private val _livesLeft = MutableLiveData<Int>(8)
+    val livesLeft: LiveData<Int>
+        get() = _livesLeft
+    private val _gameOver = MutableLiveData<Boolean>(false)
+    val gameOver: LiveData<Boolean>
+        get() = _gameOver
+
+    init {
+        _secretWordDisplay.value = deriveSecretWordDisplay()
+    }
+    fun finishGame(){
+        _gameOver.value=true
+    }
+
+    fun deriveSecretWordDisplay() : String {
+        var display = ""
+        secretWord.forEach {
+            display += checkLetter(it.toString())
+        }
+        return display
+    }
+
+    fun checkLetter(str: String) = when (correctGuesses.contains(str)) {
+        true -> str
+        false -> "_"
+    }
+
+    fun makeGuess(guess: String) {
+        if (guess.length == 1) {
+            if (secretWord.contains(guess)) {
+                correctGuesses += guess
+                _secretWordDisplay.value = deriveSecretWordDisplay()
+            } else {
+                _incorrectGuesses.value += "$guess "
+                _livesLeft.value=livesLeft.value?.minus(1)
+            }
+            if(isWon() || isLost()) _gameOver.value=true
+        }
+    }
+
+    fun isWon() = secretWord.equals(secretWordDisplay.value, true)
+
+    fun isLost() = livesLeft.value ?:0 <= 0
+
+    fun wonLostMessage() : String {
+        var message = ""
+        if (isWon()) message = "You won!"
+        else if (isLost()) message = "You lost!"
+        message += " The word was $secretWord."
+        return message
+    }
+}
---
+++ app/src/main/java/com/example/guessinggame/ResultViewModel.kt
@@ -0,0 +1,5 @@
+package com.example.guessinggame
+import androidx.lifecycle.ViewModel
+class ResultViewModel(finalResult:String): ViewModel() {
+    val result=finalResult
+}
